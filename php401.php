17aa41bf48b601bad04f4b0cd57e72e3ad9994e6
nalynq9 <janisdinosaur@gmail.com>
2021-11-23 17:32:52 +0700

    Update v1

--- php401.php
+++ php401.php
@@ -1,3 +1,34 @@
 <?php
+    function welcome(){
+        echo "Hi, Welcome to Thailand";
+    }
 
-echo "Hello Week4";
+    function sayHi($name,$age){
+        //echo "Hello week4  <br /> ";
+        echo "$name $age";
+ 
+    }
+    //Call By Reference
+    function adder(&$str,&$std1){
+        $str = "Call By Reference";
+        $std1= "James";
+    }
+    //Default argument Value
+    function lastName($name="Sakaberg"){
+        echo "Hello $name <br />";
+
+    }
+
+    welcome();           //calling a Functuon
+    echo "<br />";
+    sayHi("Mark",25);    //calling by value
+    echo "<br />";
+    sayD1("John",30);    //calling by value
+    echo "<br />";
+    adder($str,$std1);
+    echo $str," ",$std1;
+    echo "<br />";
+    echo lastName();
+
+
+?>
---
+++ php404.php
@@ -0,0 +1,7 @@
+<?php
+function inc($i){
+    echo ++$i;
+}
+
+$i = 10;
+inc($i);
